b0a632b471a8aadae093450c56d47ef3ae12d54e
Andrzej Nicpoń <andrzejmd66@gmail.com>
2018-03-26 18:53:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,28 +10,18 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- var price = Math.floor(Math.random() * 100);
- cart.push({[item] : price});
- console.log(item + " has been added to your cart.");
- return cart;
-}
-
-function viewCart() {
   if (cart.length === 0) {
-    return "Your shopping cart is empty.";
-  }
-  else {
-    var newArr = [];
-        for (var i = 0; i < cart.length; i++) {
-          var price = Object.keys(cart[i]);
-          for (var price in cart[i]) {
-            newArr.push(`${Object.keys(cart[i])} at $${cart[i][price]}`)
+      console.log("Your shopping cart is empty.")
+    } else {
+      var items = [];
+      for (var i = 0; i < cart.length; i++) {
+        for (var item in cart[i]) {
+          items.push(item + " at $" + cart[i][item])
+        }
       }
-    }
-    return console.log("In your cart, you have " + newArr.join(", ") + ".");
+      console.log("In your cart, you have " + items.join(", ") + ".");
     }
   }
-
 
 
 function total() {
