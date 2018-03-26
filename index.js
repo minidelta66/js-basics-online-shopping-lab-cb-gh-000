42472ccb22245d2de34389e8e47209ba3ed09d6c
Andrzej Nicpoń <andrzejmd66@gmail.com>
2018-03-26 18:47:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  var price = Math.floor(Math.random() * 100);
- cart.push({[item] : proce});
+ cart.push({[item] : price});
  console.log(item + " has been added to your cart.");
  return cart;
 }
